37d0afbd8b8aa8d3a7f4eee8b69b3cc6f166c89b
tedzorn <theodorezorn2020@u.northwestern.edu>
2021-02-17 15:54:58 -0500

    updated entire app

--- gg_api.py
+++ gg_api.py
@@ -3,28 +3,37 @@ import nltk
 import json
 import random
 from statistics import mode
+from collections import Counter
+import itertools
+import operator
+
 
 # global variable for holding tweets, key: year, value: list of cleaned tweets
 tweetdict = {}
 OFFICIAL_AWARDS_1315 = ['cecil b. demille award', 'best motion picture - drama', 'best performance by an actress in a motion picture - drama', 'best performance by an actor in a motion picture - drama', 'best motion picture - comedy or musical', 'best performance by an actress in a motion picture - comedy or musical', 'best performance by an actor in a motion picture - comedy or musical', 'best animated feature film', 'best foreign language film', 'best performance by an actress in a supporting role in a motion picture', 'best performance by an actor in a supporting role in a motion picture', 'best director - motion picture', 'best screenplay - motion picture', 'best original score - motion picture', 'best original song - motion picture', 'best television series - drama', 'best performance by an actress in a television series - drama', 'best performance by an actor in a television series - drama', 'best television series - comedy or musical', 'best performance by an actress in a television series - comedy or musical', 'best performance by an actor in a television series - comedy or musical', 'best mini-series or motion picture made for television', 'best performance by an actress in a mini-series or motion picture made for television', 'best performance by an actor in a mini-series or motion picture made for television', 'best performance by an actress in a supporting role in a series, mini-series or motion picture made for television', 'best performance by an actor in a supporting role in a series, mini-series or motion picture made for television']
 OFFICIAL_AWARDS_1819 = ['best motion picture - drama', 'best motion picture - musical or comedy', 'best performance by an actress in a motion picture - drama', 'best performance by an actor in a motion picture - drama', 'best performance by an actress in a motion picture - musical or comedy', 'best performance by an actor in a motion picture - musical or comedy', 'best performance by an actress in a supporting role in any motion picture', 'best performance by an actor in a supporting role in any motion picture', 'best director - motion picture', 'best screenplay - motion picture', 'best motion picture - animated', 'best motion picture - foreign language', 'best original score - motion picture', 'best original song - motion picture', 'best television series - drama', 'best television series - musical or comedy', 'best television limited series or motion picture made for television', 'best performance by an actress in a limited series or a motion picture made for television', 'best performance by an actor in a limited series or a motion picture made for television', 'best performance by an actress in a television series - drama', 'best performance by an actor in a television series - drama', 'best performance by an actress in a television series - musical or comedy', 'best performance by an actor in a television series - musical or comedy', 'best performance by an actress in a supporting role in a series, limited series or motion picture made for television', 'best performance by an actor in a supporting role in a series, limited series or motion picture made for television', 'cecil b. demille award']
 
+NAMES = nltk.corpus.names.words('male.txt') + nltk.corpus.names.words('female.txt')
+LOWERED_NAMES = [x.lower() for x in NAMES]
+
 def loadTweet(filename):
     file = open(filename)
     raw_json = json.load(file)
     tweets = []
     for item in raw_json:
-        tweets.append(clean_up_tweets(item['text'].lower()))
+        tweets.append(item['text'])
     return tweets[:]
 
 def test_sample(tweets, n):
     return random.sample(tweets,n)
 
+#takes all punctuation out of tweets
 def clean_up_tweets(tweets):
     alphabet = set('abcdefghijklmnopqrstuvwxyz ABCDEFGHIJKLMNOPQRSTUVWXYZ')
     cleaned = ''.join(filter(alphabet.__contains__, tweets))
     return cleaned 
 
+#removes retweets
 def remove_rts(tweets):
     for i in tweets[:]:
         if i.startswith('rt'):
@@ -35,15 +44,13 @@ def remove_rts(tweets):
 def get_relevant_host_tweets(year):
     '''Hosts is a list of one or more strings. Do NOT change the name
     of this function or what it returns.'''
-
     host_tweets = []
-
     tweets = tweetdict[year]
-    tweets_lowered = [clean_up_tweets(x.lower()) for x in tweets]
+    #tweets_lowered = [x for x in tweets]
     
-    hosts1 = [ i for i in tweets_lowered if "host" in i ]
-    hosts2 = [ i for i in tweets_lowered if "hosts" in i ]
-    hosts3 = [ i for i in tweets_lowered if "hosting" in i ]
+    hosts1 = [ i for i in tweets if "host" in i ]
+    hosts2 = [ i for i in tweets if "hosts" in i ]
+    hosts3 = [ i for i in tweets if "hosting" in i ]
     
     host_tweets = hosts1 + hosts2 + hosts3
     
@@ -59,55 +66,26 @@ def get_hosts(year):
     candidates = []
     # Iterate through tweets, detecting names for each one
     for tweet in tweets:
-        tokens = nltk.word_tokenize(tweet.lower())
-        if any(name in tokens for name in LOWERED_NAMES):
-            idx = tokens.index(name)
-            hosts.append(tokens[idx])
-                    
-            #hosts = Counter(candidates).most_common(2)
+        extracted = extract_entities(tweet)
+        for e in extracted:
+            if ' ' in e:
+                candidates.append(e)
+        #tokens = nltk.word_tokenize(tweet.lower())
+        #if any(name in tokens for name in LOWERED_NAMES):
+            #idx = tokens.index(name)
+        candidates.extend(extracted)
+        
+    hosts.append(Counter(candidates).most_common(2)[0][0])
+    hosts.append(Counter(candidates).most_common(2)[1][0])
     return hosts
-
-def get_relevant_award_tweets(year):
-    drama = []
-    tweets = tweetdict[year]
-    tweets_lowered = [clean_up_tweets(x.lower()) for x in tweets]
-    
-    drama1 = [ i for i in tweets_lowered if "best" in i]
-    
-    drama = drama1
-    
-    
-    
-    return drama
-
-
-# def get_drama_awards(year):
-#     drama = []
-#     tweets = tweetdict[year]
-#     tweets_lowered = [clean_up_tweets(x.lower()) for x in tweets]
-#     drama1 = [ i for i in tweets_lowered if "drama" in i]
-    
-#     drama = drama1
-    
-#     return drama
-
-# def get_comedy_or_musical_awards(year):
-#     comedy_or_musical = []
-#     tweets = tweetdict[year]
-#     tweets_lowered = [clean_up_tweets(x.lower()) for x in tweets]
-    
-#     comedy_or_musical1 = [ i for i in tweets_lowered if "comedy or musical" in i]
-    
-#     comedy_or_musical = comedy_or_musical1
-    
-#     return comedy_or_musical
 
 
 def get_awards(year):
     '''Awards is a list of strings. Do NOT change the name
     of this function or what it returns.'''
     awards = []
-    # Your code here
+    
+    
     
     return awards
 
@@ -118,6 +96,22 @@ def get_nominees(year):
     nominees = {}
     # Your code here
     return nominees
+
+def get_relevant_award_tweets(award, year):
+    relevant_tweets=[]
+    for t in tweetdict[year]:
+        if get_similarity(award, t)>0.4:
+            candidates.append(t)
+    return relevant_tweets
+
+def get_similarity(award, tweet):
+    tweet_tokens = nltk.word_tokenize(tweet)
+    award_tokens = nltk.word_tokenize(award)
+    count=0
+    for t in award_tokens:
+        if t in tweet_tokens:
+            count+=1
+    return count/len(award_tokens)
 
 def get_winner(year):
     '''Winners is a dictionary with the hard coded award
@@ -168,28 +162,26 @@ def get_presenters(year):
     # Your code here
     return presenters
 
-def extract_text(tweet):
-    return tweet['text']#.lower()
-
 def extract_entities(text):
     tokens = nltk.word_tokenize(text)
     chunks = nltk.chunk.ne_chunk(nltk.pos_tag(tokens))
     entities = []
     for c in chunks:
         if type(c) is not tuple:
-            entities.append(' '.join(leaf[0] for leaf in c.leaves()))
+            entity=' '.join(leaf[0] for leaf in c.leaves())
+            if entity!='GoldenGlobes':
+                entities.append(entity)
+            #entities.append(' '.join(leaf[0] for leaf in c.leaves()))
     return entities
 
 def make_tweet_dict():
     # Makes a dictionary of all tweets with the year as the key
     tweetdict = {}
-
     tweetdict[2013] = loadTweet('gg2013.json')
     tweetdict[2015] = loadTweet('gg2015.json')
-
     # smaller sample for testing
-    tweetdict[2013] = test_sample(tweetdict[2013],2000)
-    tweetdict[2015] = test_sample(tweetdict[2015],2000)
+    #tweetdict[2013] = test_sample(tweetdict[2013],2000)
+    #tweetdict[2015] = test_sample(tweetdict[2015],2000)
     return tweetdict
 
 def pre_ceremony():
@@ -214,12 +206,28 @@ def main():
     # Your code here
     # Make a dictionary of all tweets
     pre_ceremony()
-    print(remove_rts(tweetdict[2013]))
-
+    get_relevant_award_tweets(2013)
 
     
 
 #if __name__ == '__main__':
 #    main()
-main()
+#main()
+
+#print(NAMES)
+pre_ceremony()
+get_similarity('cecil b. demille award', tweetdict[2013][29])
+
+#print(get_hosts(2013))
+#original = ['Congratulations to Seth Meyers is hosting the GoldenGlobes', 
+#                        'This is a frog named Jim',
+#                       'seth meyers hosts the golden globes',
+#           'Seth Meyers is the host of the goldenglobes']
+
+#output_list = [extract_entities(i) for i in original] 
+#print(output_list)
+
+#extensionsToCheck = ['.pdf', '.doc', '.xls']
+#print(extensionsToCheck[1])
+
 
