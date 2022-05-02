9e0977e7b35eb83023f616b14cc0b291a2778999
Allison Rhee <77465589+allisonjrhee@users.noreply.github.com>
2022-05-02 13:44:13 -0500

    adding addtl func

--- gg_api.py
+++ gg_api.py
@@ -1,7 +1,18 @@
-'''Version 0.35'''
+import re
+import json
+import math
+import nltk
+from nltk.corpus import stopwords as sw
+from nltk.tokenize import wordpunct_tokenize
+from nltk.sentiment import SentimentIntensityAnalyzer
 
 OFFICIAL_AWARDS_1315 = ['cecil b. demille award', 'best motion picture - drama', 'best performance by an actress in a motion picture - drama', 'best performance by an actor in a motion picture - drama', 'best motion picture - comedy or musical', 'best performance by an actress in a motion picture - comedy or musical', 'best performance by an actor in a motion picture - comedy or musical', 'best animated feature film', 'best foreign language film', 'best performance by an actress in a supporting role in a motion picture', 'best performance by an actor in a supporting role in a motion picture', 'best director - motion picture', 'best screenplay - motion picture', 'best original score - motion picture', 'best original song - motion picture', 'best television series - drama', 'best performance by an actress in a television series - drama', 'best performance by an actor in a television series - drama', 'best television series - comedy or musical', 'best performance by an actress in a television series - comedy or musical', 'best performance by an actor in a television series - comedy or musical', 'best mini-series or motion picture made for television', 'best performance by an actress in a mini-series or motion picture made for television', 'best performance by an actor in a mini-series or motion picture made for television', 'best performance by an actress in a supporting role in a series, mini-series or motion picture made for television', 'best performance by an actor in a supporting role in a series, mini-series or motion picture made for television']
 OFFICIAL_AWARDS_1819 = ['best motion picture - drama', 'best motion picture - musical or comedy', 'best performance by an actress in a motion picture - drama', 'best performance by an actor in a motion picture - drama', 'best performance by an actress in a motion picture - musical or comedy', 'best performance by an actor in a motion picture - musical or comedy', 'best performance by an actress in a supporting role in any motion picture', 'best performance by an actor in a supporting role in any motion picture', 'best director - motion picture', 'best screenplay - motion picture', 'best motion picture - animated', 'best motion picture - foreign language', 'best original score - motion picture', 'best original song - motion picture', 'best television series - drama', 'best television series - musical or comedy', 'best television limited series or motion picture made for television', 'best performance by an actress in a limited series or a motion picture made for television', 'best performance by an actor in a limited series or a motion picture made for television', 'best performance by an actress in a television series - drama', 'best performance by an actor in a television series - drama', 'best performance by an actress in a television series - musical or comedy', 'best performance by an actor in a television series - musical or comedy', 'best performance by an actress in a supporting role in a series, limited series or motion picture made for television', 'best performance by an actor in a supporting role in a series, limited series or motion picture made for television', 'cecil b. demille award']
+
+stopwords = sw.words("english")
+
+def tokenize(text):
+    return [t.lower() for t in wordpunct_tokenize(text)]
 
 def get_hosts(year):
     '''Hosts is a list of one or more strings. Do NOT change the name
@@ -36,6 +47,23 @@ def get_presenters(year):
     # Your code here
     return presenters
 
+def sentiment(year):
+    sia = SentimentIntensityAnalyzer()
+    siaps = sia.polarity_scores("Wow, NLTK is really powerful!")
+    # print(siaps)
+
+    def is_positive(tweet: str) -> bool:
+        """True if tweet has positive compound sentiment, False otherwise."""
+        return sia.polarity_scores(tweet)["compound"] > 0
+
+    # for tweet in tweets:
+    #     print(">", is_positive(tweet), tweet)
+    return sentiment
+
+def bestdressed(year):
+    hashtag = "#redcarpet"
+    return bestdressed
+
 def pre_ceremony():
     '''This function loads/fetches/processes any data your program
     will use, and stores that data in your DB or in a json, csv, or
@@ -53,6 +81,9 @@ def main():
     what it returns.'''
     # Your code here
     return
+
+
+
 
 if __name__ == '__main__':
     main()
