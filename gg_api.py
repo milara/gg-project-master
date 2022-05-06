599fdfb4679f73389bc024e61bdfad3ba60f4fc8
Allison Rhee <77465589+allisonjrhee@users.noreply.github.com>
2022-05-05 19:17:50 -0500

    fix host return

--- gg_api.py
+++ gg_api.py
@@ -14,10 +14,34 @@ stopwords = sw.words("english")
 def tokenize(text):
     return [t.lower() for t in wordpunct_tokenize(text)]
 
+def get_tweets(year):
+    with open('gg{}.json'.format(year)) as f:
+        tweet_information = json.load(f)
+
+        tweet_text_lst = []
+        for tweet in tweet_information:
+            tweet_text_lst.append(tweet['text'])
+
+    return tweet_text_lst
+
 def get_hosts(year):
     '''Hosts is a list of one or more strings. Do NOT change the name
     of this function or what it returns.'''
-    # Your code here
+    host_discussion_pattern = re.compile('hosted|hosting')
+    host_relevant_tweets = list(filter(host_discussion_pattern.search, get_tweets(year)))
+
+    host_multiple_names_pattern = re.compile('[A-Z][a-z]+ [A-Z][a-z]+ (and|&) [A-Z][a-z]+ [A-Z][a-z]+')
+    host_mention_tweets = list(filter(host_multiple_names_pattern.search, host_relevant_tweets))
+
+    host_name_pattern = re.compile('[A-Z][a-z]+ [A-Z][a-z]+')
+    host_names = Counter()
+    for tweet in host_mention_tweets:
+        potential_names = host_name_pattern.findall(tweet)
+        host_names.update(potential_names)
+    hosts = []
+    lstofhosts = host_names.most_common(2)
+    for n,f in lstofhosts:
+        hosts.append(n)
     return hosts
 
 def get_awards(year):
