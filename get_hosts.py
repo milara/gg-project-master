af25bb18417cb19081a91bd3ffaf01e90cc79917
Derex Wangmang <66037338+derexwangmang@users.noreply.github.com>
2022-05-05 19:08:43 -0500

    Implemented get hosts

--- get_hosts.py
+++ get_hosts.py
@@ -13,17 +13,19 @@ def get_tweets(year):
     return tweet_text_lst
 
 def get_hosts(year):
-    host_discussion_pattern = re.compile('host')
+    host_discussion_pattern = re.compile('hosted|hosting')
     host_relevant_tweets = list(filter(host_discussion_pattern.search, get_tweets(year)))
 
-    host_name_pattern = re.compile('[A-Z][a-z]+ [A-Z][a-z]+')
-    host_mention_tweets = list(filter(host_name_pattern.search, host_relevant_tweets))
+    host_multiple_names_pattern = re.compile('[A-Z][a-z]+ [A-Z][a-z]+ (and|&) [A-Z][a-z]+ [A-Z][a-z]+')
+    host_mention_tweets = list(filter(host_multiple_names_pattern.search, host_relevant_tweets))
 
+    host_name_pattern = re.compile('[A-Z][a-z]+ [A-Z][a-z]+')
     host_names = Counter()
     for tweet in host_mention_tweets:
         potential_names = host_name_pattern.findall(tweet)
         host_names.update(potential_names)
     
-    print(host_names.most_common(10))
+    return host_names.most_common(2)
 
-get_hosts('2013')
+print('2013: ', get_hosts('2013'))
+print('2015: ', get_hosts('2015'))
