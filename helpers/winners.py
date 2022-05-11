7c05dc5c01f3ba963e033482fc016b6653ad9c5f
Derex Wangmang <66037338+derexwangmang@users.noreply.github.com>
2022-05-10 22:30:54 -0500

    Pair programmed to work on getting winners

--- helpers/winners.py
+++ helpers/winners.py
@@ -1,115 +1,75 @@
 import json
 import re
+import nltk
 from collections import Counter
-import helpers.awards
-import awards
+from difflib import SequenceMatcher
+# from nltk.corpus import stopwords
 
-from nltk.corpus import stopwords as sw
-from nltk.tokenize import wordpunct_tokenize
-from nltk.sentiment import SentimentIntensityAnalyzer
+# stop_words = set(stopwords.words('english'))
 
 OFFICIAL_AWARDS_1315 = ['cecil b. demille award', 'best motion picture - drama', 'best performance by an actress in a motion picture - drama', 'best performance by an actor in a motion picture - drama', 'best motion picture - comedy or musical', 'best performance by an actress in a motion picture - comedy or musical', 'best performance by an actor in a motion picture - comedy or musical', 'best animated feature film', 'best foreign language film', 'best performance by an actress in a supporting role in a motion picture', 'best performance by an actor in a supporting role in a motion picture', 'best director - motion picture', 'best screenplay - motion picture', 'best original score - motion picture', 'best original song - motion picture', 'best television series - drama', 'best performance by an actress in a television series - drama', 'best performance by an actor in a television series - drama', 'best television series - comedy or musical', 'best performance by an actress in a television series - comedy or musical', 'best performance by an actor in a television series - comedy or musical', 'best mini-series or motion picture made for television', 'best performance by an actress in a mini-series or motion picture made for television', 'best performance by an actor in a mini-series or motion picture made for television', 'best performance by an actress in a supporting role in a series, mini-series or motion picture made for television', 'best performance by an actor in a supporting role in a series, mini-series or motion picture made for television']
 OFFICIAL_AWARDS_1819 = ['best motion picture - drama', 'best motion picture - musical or comedy', 'best performance by an actress in a motion picture - drama', 'best performance by an actor in a motion picture - drama', 'best performance by an actress in a motion picture - musical or comedy', 'best performance by an actor in a motion picture - musical or comedy', 'best performance by an actress in a supporting role in any motion picture', 'best performance by an actor in a supporting role in any motion picture', 'best director - motion picture', 'best screenplay - motion picture', 'best motion picture - animated', 'best motion picture - foreign language', 'best original score - motion picture', 'best original song - motion picture', 'best television series - drama', 'best television series - musical or comedy', 'best television limited series or motion picture made for television', 'best performance by an actress in a limited series or a motion picture made for television', 'best performance by an actor in a limited series or a motion picture made for television', 'best performance by an actress in a television series - drama', 'best performance by an actor in a television series - drama', 'best performance by an actress in a television series - musical or comedy', 'best performance by an actor in a television series - musical or comedy', 'best performance by an actress in a supporting role in a series, limited series or motion picture made for television', 'best performance by an actor in a supporting role in a series, limited series or motion picture made for television', 'cecil b. demille award']
 
-stopwords = sw.words("english")
+def similiar(a,b):
+    if SequenceMatcher(None, a, b).ratio() >= 0.70:
+        return True
+    else:
+        return False
 
-def tokenize(text):
-    return [t.lower() for t in wordpunct_tokenize(text)]
-
+# Returns all tweet text from year in lowercase without stop words
 def get_tweets(year):
     with open('gg{}.json'.format(year)) as f:
         tweet_information = json.load(f)
 
         tweet_text_lst = []
         for tweet in tweet_information:
+            # lowered_text = tweet['text'].lower()
+            # text_lst = nltk.tokenize.word_tokenize(lowered_text)
+            # filtered_text_lst = [token for token in text_lst if token not in stop_words]
+            # tweet_text_lst.append(' '.join(filtered_text_lst))
             tweet_text_lst.append(tweet['text'])
 
     return tweet_text_lst
 
-def get_hosts(year):
-    '''Hosts is a list of one or more strings. Do NOT change the name
-    of this function or what it returns.'''
-    host_discussion_pattern = re.compile('hosted|hosting')
-    host_relevant_tweets = list(filter(host_discussion_pattern.search, get_tweets(year)))
-
-    host_multiple_names_pattern = re.compile('[A-Z][a-z]+ [A-Z][a-z]+ (and|&) [A-Z][a-z]+ [A-Z][a-z]+')
-    host_mention_tweets = list(filter(host_multiple_names_pattern.search, host_relevant_tweets))
-
-    host_name_pattern = re.compile('[A-Z][a-z]+ [A-Z][a-z]+')
-    host_names = Counter()
-    for tweet in host_mention_tweets:
-        potential_names = host_name_pattern.findall(tweet)
-        host_names.update(potential_names)
-    hosts = []
-    lstofhosts = host_names.most_common(2)
-    for n,f in lstofhosts:
-        hosts.append(n)
-    return hosts
-
-def get_awards(year):
-    '''Awards is a list of strings. Do NOT change the name
-    of this function or what it returns.'''
-    # Your code here
-    return awards.get_award(year)
-
-def get_nominees(year):
-    '''Nominees is a dictionary with the hard coded award
-    names as keys, and each entry a list of strings. Do NOT change
-    the name of this function or what it returns.'''
-    # Your code here
-    return {}
-
 def get_winner(year):
-    '''Winners is a dictionary with the hard coded award
-    names as keys, and each entry containing a single string.
-    Do NOT change the name of this function or what it returns.'''
-    # Your code here
-    return {}
+    if year == '2013':
+        award_mappings = {}
+        with open('./2013categories.txt') as file:
+            lines = file.readlines()
+            for line in lines:
+                award_mappings[line] = None
+    
+    all_tweets = get_tweets(year)
+    potential_winners = {}
+    for award in award_mappings.keys():
+        potential_winners[award] = Counter()
 
-def get_presenters(year):
-    '''Presenters is a dictionary with the hard coded award
-    names as keys, and each entry a list of strings. Do NOT change the
-    name of this function or what it returns.'''
-    # Your code here
-    return {}
+    won_pattern = re.compile('got|won|wins|is awarded')
+    tweets_containing_won = list(filter(won_pattern.search, get_tweets(year)))
+    
+    for tweet in tweets_containing_won:
+        splitted_tweet = won_pattern.split(tweet)
+        potential_winner = splitted_tweet[0]
+        potential_award = splitted_tweet[1]
 
-def sentiment(year):
-    sia = SentimentIntensityAnalyzer()
-    siaps = sia.polarity_scores("Wow, NLTK is really powerful!")
-    # print(siaps)
+        for award in award_mappings.keys():
+            if award in potential_award:   
+                splitted_potential_winner = potential_winner.split()             
+                # Backwards count from award
+                for i in range(len(potential_winner) - 1, 0, -1):
+                    temp = ' '.join(splitted_potential_winner[i+1:])
+                    potential_winners[award][temp] = potential_winners[award].get(temp, 0) + 1
 
-    def is_positive(tweet: str) -> bool:
-        """True if tweet has positive compound sentiment, False otherwise."""
-        return sia.polarity_scores(tweet)["compound"] > 0
+                # Forwards count to award
+                for i in range(len(splitted_potential_winner)):
+                    temp = ' '.join(splitted_potential_winner[:i+1])
+                    potential_winners[award][temp] = potential_winners[award].get(temp, 0) + 1
 
-    # for tweet in tweets:
-    #     print(">", is_positive(tweet), tweet)
-    return sentiment
+    for award in potential_winners.keys():
+        print(award, potential_winners[award].most_common(10))
+    # for award in award_mappings.keys():
+    #     award_mappings[award] = potential_winners[award].most_common(10)
 
-def bestdressed(year):
-    hashtag = "#redcarpet"
-    return bestdressed
+    # return award_mappings
 
-def pre_ceremony():
-    '''This function loads/fetches/processes any data your program
-    will use, and stores that data in your DB or in a json, csv, or
-    plain text file. It is the first thing the TA will run when grading.
-    Do NOT change the name of this function or what it returns.'''
-    # Your code here
-    print("Pre-ceremony processing complete.")
-    return
-
-def main():
-    '''This function calls your program. Typing "python gg_api.py"
-    will run this function. Or, in the interpreter, import gg_api
-    and then run gg_api.main(). This is the second thing the TA will
-    run when grading. Do NOT change the name of this function or
-    what it returns.'''
-    # Your code here
-    return
-
-
-
-
-if __name__ == '__main__':
-    main()
+get_winner('2013')
